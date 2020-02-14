845f6025474d2d319c761ae1670874b62795b82b
RaviBlock8 <ravi83165@gmail.com>
2020-02-14 15:21:37 +0530

    commit

--- contracts/DappTokenCrowdSale.sol
+++ contracts/DappTokenCrowdSale.sol
@@ -4,19 +4,68 @@ import "../node_modules/@openzeppelin/contracts/crowdsale/Crowdsale.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../node_modules/@openzeppelin/contracts/crowdsale/emission/MintedCrowdsale.sol";
 import "../node_modules/@openzeppelin/contracts/crowdsale/validation/CappedCrowdsale.sol";
+import "../node_modules/@openzeppelin/contracts/crowdsale/validation/TimedCrowdsale.sol";
+import "../node_modules/@openzeppelin/contracts/crowdsale/validation/WhitelistCrowdsale.sol";
+// import "../node_modules/@openzeppelin/contracts/crowdsale/distribution/RefundableCrowdsale.sol";
 
-contract DappTokenCrowdSale is Crowdsale,MintedCrowdsale,CappedCrowdsale{
+contract DappTokenCrowdSale is Crowdsale,MintedCrowdsale,CappedCrowdsale ,TimedCrowdsale,WhitelistCrowdsale
+{
 
+    // track investors investment
     uint256 private investorMinCap=2000000000000000000;
     uint256 private investorMaxCap=50000000000000000000;
+  
     mapping(address=>uint256) public contributions;
-    constructor(uint256 rate,
+
+    //keep track of ICO stage
+    enum CrowdsaleStage{PreICO,ICO}
+    //default to presale stage
+    CrowdsaleStage public stage=CrowdsaleStage.PreICO;
+
+
+    constructor(
+    uint256 rate,
     address payable wallet,
-    IERC20 token,uint cap)
+    IERC20 token,
+    uint cap,
+    uint _openingTime,
+    uint _closingTime
+    )
     Crowdsale(rate,wallet,token)
     CappedCrowdsale(cap)
+    TimedCrowdsale(_openingTime,_closingTime)
     public{
+        // require(_goal <= cap," goal exceeds cap");
+    }
 
+    // get user contributions
+
+    function getContributions(address _beneficiary) public view returns(uint256){
+        return contributions[_beneficiary];
+    }
+
+    //set crowdsale stage
+
+    function setCrowdsaleStage(uint _stage) public{
+        if(uint(CrowdsaleStage.PreICO)==_stage){
+            stage = CrowdsaleStage.PreICO;
+            //to enable this change of value i have changed _rate from private to internal in crowdsale.sol
+            _rate = 500;
+        }else{
+            stage = CrowdsaleStage.ICO;
+           _rate = 250;
+        }
+
+        
+    }
+
+    function _forwardFunds() internal {
+        if(stage == CrowdsaleStage.PreICO){
+            wallet().transfer(msg.value);
+        }else if(stage == CrowdsaleStage.ICO){
+            super._forwardFunds();
+        }
+        
     }
 
     function preValidatePurchase(address _beneficiary,uint _weiAmount) internal{
--- test/DappTokenCrowdsale.test.js
+++ test/DappTokenCrowdsale.test.js
@@ -17,9 +17,15 @@ contract('DappTokenCrowdsale',([_,wallet,investor1,investor2])=>{
         this.rate=500
         this.wallet=wallet
         this.cap=20000
-        this.crowd=await DappTokenCrowdSale.new(this.rate,this.wallet,this.token.address,new BN(this.cap,2))
+        
+        this.openingTime=Math.round((new Date()).getTime() / 1000);
+        this.closingTime=1781502943
+        this.crowd=await DappTokenCrowdSale.new(this.rate,this.wallet,this.token.address,new BN(this.cap,2),this.openingTime,this.closingTime)
         await this.token.addMinter(this.crowd.address)
+        await this.crowd.addWhitelisted(investor1)
         // console.log('ownerhsip transfered')
+        this.vaultAddress=await this.crowd.vault()
+        console.log('vault address:',this.vaultAddress)
 
     })
     describe('crowdsale',()=>{
@@ -51,9 +57,71 @@ contract('DappTokenCrowdsale',([_,wallet,investor1,investor2])=>{
     describe("check crowdsale function",()=>{
         it("check cap value",async ()=>{
             let _cap=await this.crowd.cap()
+            _cap=_cap.toNumber()
             _cap.should.equal(this.cap)
         })
     })
+
+    describe("check timed crowdsale deployement",()=>{
+        it("check opening time",async()=>{
+            let _openingTime=await this.crowd.openingTime()
+            _openingTime=_openingTime.toNumber()
+            _openingTime.should.equal(this.openingTime)
+        })
+    })
+
+
+    describe("white list crowdsale",()=>{
+        it("check if person is whitelisted",async()=>{
+            let res=await this.crowd.isWhitelisted(investor1)
+            res.should.equal(true)
+        })
+
+        it("check if this non-whitelisted person is whitelist",async()=>{
+            let res=await this.crowd.isWhitelisted(investor2)
+            res.should.equal(false)
+        })
+
+        describe("accepting payments",()=>{
+            it("shouldn't accept payment from non-whitelist person",async ()=>{
+            // let _value=ether(1);
+            await this.crowd.sendTransaction({value:'100',from:investor2}).should.be.rejected
+        })
+    })
+    })
+
+    describe("crowdsale stages",()=>{
+        it("check stage is preIco",async()=>{
+            let stage=await this.crowd.stage();
+            stage=stage.toNumber()
+            stage.should.equal(0);
+        })
+    })
+
+    describe("check where money goes in PreICO stage",()=>{
+        beforeEach(async ()=>{
+            await this.crowd.setCrowdsaleStage(0,{from:investor1})
+            await this.crowd.sendTransaction({value:'100',from:investor1}).should.be.fulfilled
+        })
+
+        it("check where is money",async ()=>{
+            const balnc=await web3.eth.getBalance(this.wallet)
+            console.log('balance of wallet',balnc)
+        })
+    })
+
+    describe("crowdsale stage after changing",()=>{
+        it("check stage is preIco",async()=>{
+            await this.crowd.setCrowdsaleStage(1,{from:investor1})
+            let stage=await this.crowd.stage();
+            stage=stage.toNumber()
+            stage.should.equal(1);
+        })
+    })
+
+    
+
+    
 
 
 
